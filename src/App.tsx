a17bbb89c32c5401fc176e2654b9ec30f7476ad7
MrBiedny <MrBiedny@gmail.com>
2024-09-22 23:48:49 +0200

    add new sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Download from "./sections/Download";
+import Faq from "./sections/Faq";
 import Features from "./sections/Features";
+import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonials";
 
 export default function App() {
   return (
@@ -10,6 +14,10 @@ export default function App() {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 }
