4ae5b9cbcbf61dc980caa64859e7ad2cbd870aea
MrBiedny <MrBiedny@gmail.com>
2024-09-22 18:31:49 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Features from "./sections/Features";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
+
+export default function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+    </main>
+  );
+}
